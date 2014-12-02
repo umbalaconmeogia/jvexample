0cb2ee96945c1af5cbefd9c852c9dd516c6248b8
Tran Trung Thanh <umbalaconmeogia@gmail.com>
2014-12-02 17:21:31 +0900

    Add db (sqlite)

--- web/config/db.php
+++ web/config/db.php
@@ -2,8 +2,5 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'dsn' => 'sqlite:' . __DIR__ . '/../db/jvexample.db',
 ];
---
+++ web/db/schema.sqlite.bat
@@ -0,0 +1,1 @@
+sqlite3 jvexample.db < schema.sqlite.sql
